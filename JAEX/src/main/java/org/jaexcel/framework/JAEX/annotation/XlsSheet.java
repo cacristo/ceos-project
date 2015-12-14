a55a396bfac346953f72d70bced8c7688b0d9f5c
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2015-12-14 20:33:48 +0100

    - Add new attribute 'cascadeLevel' moved from @XlsConfiguration - JavaDoc

--- JAEX/src/main/java/org/jaexcel/framework/JAEX/annotation/XlsSheet.java
+++ JAEX/src/main/java/org/jaexcel/framework/JAEX/annotation/XlsSheet.java
@@ -6,28 +6,52 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import org.jaexcel.framework.JAEX.definition.CascadeType;
 import org.jaexcel.framework.JAEX.definition.PropagationType;
 
-//@Target({ TYPE })
+/**
+ * Annotation responsible to manage the basic Sheet configuration.
+ * 
+ * @author CristoAbreu
+ */
 @Target(TYPE)
 @Retention(RUNTIME)
 public @interface XlsSheet {
 
+	// FIXME pass the attribute at @XlsConfiguration to this annotation
+
 	/**
 	 * Define the sheet title.
 	 * 
-	 * @return
+	 * @return the title
 	 */
 	String title();
 
 	/**
-	 * Define the propagation type when writing at he Excel.
+	 * Define the row start position.
+	 * 
+	 * @return by default is 1.
+	 */
+	int startRow() default 1;
+
+	/**
+	 * Define the cell start position.
+	 * 
+	 * @return by default is 1.
+	 */
+	int startCell() default 1;
+
+	/**
+	 * Define the propagation type when writing/reading at the Excel.
 	 * 
 	 * @return the {@link PropagationType}, by default is PROPAGATION_HORIZONTAL
 	 */
 	PropagationType propagation() default PropagationType.PROPAGATION_HORIZONTAL;
 
-	int startRow() default 1;
-
-	int startCell() default 1;
+	/**
+	 * Define the cascade level.
+	 * 
+	 * @return the {@link CascadeType}, by default is CASCADE_BASE
+	 */
+	CascadeType cascadeLevel() default CascadeType.CASCADE_BASE;
 }
