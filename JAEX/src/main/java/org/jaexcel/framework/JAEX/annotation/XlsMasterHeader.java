81e2a3ea7753271e35b8d6b9243b861bdd3ffc2d
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2015-12-14 20:34:46 +0100

    JavaDoc

--- JAEX/src/main/java/org/jaexcel/framework/JAEX/annotation/XlsMasterHeader.java
+++ JAEX/src/main/java/org/jaexcel/framework/JAEX/annotation/XlsMasterHeader.java
@@ -6,28 +6,57 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-//@Target({ FIELD })
+/**
+ * Annotation responsible to manage the master header at the Sheet.
+ * 
+ * @author CristoAbreu
+ */
 @Target(FIELD)
 @Retention(RUNTIME)
 public @interface XlsMasterHeader {
 	/**
-	 * Define the master cell title.
+	 * Define the master element title.
 	 * 
-	 * @return
+	 * @return the title of the element
 	 */
 	String title();
 
+	/**
+	 * Define the start cell position.
+	 * 
+	 * @return by default is 0
+	 */
 	int startX() default 0;
 
+	/**
+	 * Define the end cell position.
+	 * 
+	 * @return by default is 0
+	 */
 	int endX() default 0;
 
+	/**
+	 * Define the start row position.
+	 * 
+	 * @return by default is 0
+	 */
 	int startY() default 0;
 
+	/**
+	 * Define the end row position.
+	 * 
+	 * @return by default is 0
+	 */
 	int endY() default 0;
 
 	// org.apache.poi.ss.util.CellRangeAddress;
+	@Deprecated
 	int mergedRegion() default 0;
 
+	/**
+	 * 
+	 * @return by default is 0
+	 */
 	int decorator() default 0;
 
 	int border() default 0;
