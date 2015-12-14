29178dea907bfaa08c9b85b0fc4284b80e2bdb5f
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2015-12-14 20:30:58 +0100

    - Attribute 'cascadeLevel' moved to @XlsSheet - JavaDoc

--- JAEX/src/main/java/org/jaexcel/framework/JAEX/annotation/XlsConfiguration.java
+++ JAEX/src/main/java/org/jaexcel/framework/JAEX/annotation/XlsConfiguration.java
@@ -6,17 +6,27 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import org.jaexcel.framework.JAEX.definition.CascadeType;
 import org.jaexcel.framework.JAEX.definition.ExtensionFileType;
 
-//@Target({ TYPE })
+/**
+ * 
+ * @author CristoAbreu
+ */
 @Target(TYPE)
 @Retention(RUNTIME)
 public @interface XlsConfiguration {
 
+	/**
+	 * Define the file name.
+	 * 
+	 * @return the name of the file
+	 */
 	String nameFile();
 	
+	/**
+	 * Define the extension file.
+	 * 
+	 * @return the {@link ExtensionFileType}, by default is XLS
+	 */
 	ExtensionFileType extensionFile() default ExtensionFileType.XLS;
-
-	CascadeType cascadeLevel() default CascadeType.CASCADE_BASE;
 }
