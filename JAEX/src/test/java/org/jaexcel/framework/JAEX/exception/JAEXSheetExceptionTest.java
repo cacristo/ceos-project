ee1b475b10d086d1091ec38982c7887fd274db7c
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2015-12-15 20:16:13 +0100

    Creation of the classes to tests the exceptions

---
+++ JAEX/src/test/java/org/jaexcel/framework/JAEX/exception/JAEXSheetExceptionTest.java
@@ -0,0 +1,25 @@
+package org.jaexcel.framework.JAEX.exception;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class JAEXSheetExceptionTest extends TestCase {
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *            name of the test case
+	 */
+	public JAEXSheetExceptionTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(JAEXSheetExceptionTest.class);
+	}
+
+}
