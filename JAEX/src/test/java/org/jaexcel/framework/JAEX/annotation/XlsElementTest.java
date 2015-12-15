12c7e455dc377d0b14c0d533f798cebcbfb1235c
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2015-12-15 20:09:51 +0100

    Add new tests to XlsElementTest

--- JAEX/src/test/java/org/jaexcel/framework/JAEX/annotation/XlsElementTest.java
+++ JAEX/src/test/java/org/jaexcel/framework/JAEX/annotation/XlsElementTest.java
@@ -5,11 +5,12 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.jaexcel.framework.JAEX.bean.ObjectsBuilderTest;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
-import org.jaexcel.framework.JAEX.bean.ObjectWithDefaultConfig;
 
 public class XlsElementTest extends TestCase {
 	/**
@@ -29,8 +30,11 @@ public class XlsElementTest extends TestCase {
 		return new TestSuite(XlsElementTest.class);
 	}
 
+	/**
+	 * Test default configuration.
+	 */
 	public void testDefaultConfiguration() {
-		Class<ObjectWithDefaultConfig> oC = ObjectWithDefaultConfig.class;
+		Class<ObjectsBuilderTest.ObjectWithDefaultConfig> oC = ObjectsBuilderTest.ObjectWithDefaultConfig.class;
 
 		List<Field> fL = Arrays.asList(oC.getDeclaredFields());
 		for (Field f : fL) {
@@ -41,8 +45,77 @@ public class XlsElementTest extends TestCase {
 				XlsElement xlsElement = (XlsElement) annotation;
 
 				assertEquals(xlsElement.comment(), "");
-				assertEquals(xlsElement.cellStyle(), 0);
 				assertEquals(xlsElement.decorator(), "");
+			}
+		}
+	}
+
+	/**
+	 * Test title attribute.
+	 */
+	public void testTitleAttribute() {
+		Class<ObjectsBuilderTest.Cyclops> oC = ObjectsBuilderTest.Cyclops.class;
+
+		List<Field> fL = Arrays.asList(oC.getDeclaredFields());
+		for (Field f : fL) {
+			// Process @XlsElement
+			if (f.isAnnotationPresent(XlsElement.class)) {
+
+				Annotation annotation = f.getAnnotation(XlsElement.class);
+				XlsElement xlsElement = (XlsElement) annotation;
+
+				if(f.getName().equals("dateAttribute")){
+					assertEquals(xlsElement.title(), "Date value");
+				}
+				if(f.getName().equals("stringAttribute")){
+					assertEquals(xlsElement.title(), "String value");
+				}
+				if(f.getName().equals("integerAttribute")){
+					assertEquals(xlsElement.title(), "Integer value");
+				}
+
+			}
+		}
+	}
+
+	/**
+	 * Test comment attribute.
+	 */
+	public void testCommentAttribute() {
+		Class<ObjectsBuilderTest.Cyclops> oC = ObjectsBuilderTest.Cyclops.class;
+
+		List<Field> fL = Arrays.asList(oC.getDeclaredFields());
+		for (Field f : fL) {
+			// Process @XlsElement
+			if (f.isAnnotationPresent(XlsElement.class)) {
+
+				Annotation annotation = f.getAnnotation(XlsElement.class);
+				XlsElement xlsElement = (XlsElement) annotation;
+
+				if(StringUtils.isNotBlank(xlsElement.comment())){
+					assertEquals(xlsElement.comment(), "This is a comment");
+				}
+			}
+		}
+	}
+
+	/**
+	 * Test decorator attribute.
+	 */
+	public void testDecoratorAttribute() {
+		Class<ObjectsBuilderTest.Cyclops> oC = ObjectsBuilderTest.Cyclops.class;
+
+		List<Field> fL = Arrays.asList(oC.getDeclaredFields());
+		for (Field f : fL) {
+			// Process @XlsElement
+			if (f.isAnnotationPresent(XlsElement.class)) {
+
+				Annotation annotation = f.getAnnotation(XlsElement.class);
+				XlsElement xlsElement = (XlsElement) annotation;
+
+				if(StringUtils.isNotBlank(xlsElement.decorator())){
+					assertEquals(xlsElement.decorator(), "0.0");
+				}
 			}
 		}
 	}
