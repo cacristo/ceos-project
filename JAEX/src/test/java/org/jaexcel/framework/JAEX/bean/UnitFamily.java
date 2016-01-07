b5eec03fc6f9672fb2648589ceb8f8ef0019cfeb
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-01-07 06:48:18 +0100

    Create enum UnitFamily

---
+++ JAEX/src/test/java/org/jaexcel/framework/JAEX/bean/UnitFamily.java
@@ -0,0 +1,20 @@
+package org.jaexcel.framework.JAEX.bean;
+
+public enum UnitFamily {
+	COMPUTER("Computer"),
+	TABLETS("Tablets"),
+	COMPONENTS("Components"),
+	STORAGE("Storage"),
+	NETWORK("Network"),
+	SOFTWARE("Software");
+
+	private String family;
+
+	private UnitFamily(String s) {
+		family = s;
+	}
+
+	public String getFamily() {
+		return family;
+	}
+}
