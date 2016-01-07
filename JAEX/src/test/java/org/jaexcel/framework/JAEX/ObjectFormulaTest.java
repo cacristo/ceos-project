e6cfd1611500219df8b23e5f7715c3daeb194bb7
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-01-07 06:59:21 +0100

    Object to test formula

---
+++ JAEX/src/test/java/org/jaexcel/framework/JAEX/ObjectFormulaTest.java
@@ -0,0 +1,50 @@
+package org.jaexcel.framework.JAEX;
+
+import org.jaexcel.framework.JAEX.bean.ObjectFormula;
+import org.jaexcel.framework.JAEX.engine.Engine;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ObjectFormulaTest extends TestCase {
+
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *            name of the test case
+	 */
+	public ObjectFormulaTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(ObjectFormulaTest.class);
+	}
+
+
+	/**
+	 * Test one basic object
+	 */
+	public void testMarshalObjectFormula() throws Exception {
+		ObjectFormula of = new ObjectFormula();
+
+		of.setStore(200);
+		of.setWebStore(455);
+
+		of.setValueLocal(1623.99);
+		of.setValueRegion(3199.99);
+		of.setValueCountry(8421.80);
+		
+		Engine en = new Engine();
+
+		en.marshal(of);
+
+		assertEquals(true, true);
+	}
+
+}
