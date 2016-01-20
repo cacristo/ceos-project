cac6c419104b72e2f0ceba7d2efd7c3ac5949c42
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-01-21 00:22:31 +0100

    Add .csv extension

--- JAEX/src/main/java/org/jaexcel/framework/JAEX/definition/ExtensionFileType.java
+++ JAEX/src/main/java/org/jaexcel/framework/JAEX/definition/ExtensionFileType.java
@@ -5,12 +5,13 @@ package org.jaexcel.framework.JAEX.definition;
  * 
  * {@value = ExtensionFileType.XLSX } Related to the files with the extension .xlsx. <br>
  * {@value = ExtensionFileType.XLS } Related to the files with the extension .xls. <br>
+ * {@value = ExtensionFileType.CSV } Related to the files with the extension .csv. <br>
  * 
  * @version 1.0
  * @author Carlos CRISTO ABREU
  */
 public enum ExtensionFileType {
-	XLSX(".xlsx"), XLS(".xls");
+	XLSX(".xlsx"), XLS(".xls"), CSV(".csv");
 
 	private String extension;
 
