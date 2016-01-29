40546b4951b51b545cbb7a7d7d37171d3656d43e
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-01-29 17:25:42 +0100

    Add new messages

--- JAEX/src/main/java/org/jaexcel/framework/JAEX/definition/JAEXExceptionMessage.java
+++ JAEX/src/main/java/org/jaexcel/framework/JAEX/definition/JAEXExceptionMessage.java
@@ -14,8 +14,13 @@ public enum JAEXExceptionMessage {
 
 	/* declared messages of JAEXConverterException */
 	JAEXConverterException_Default("Problem while convert the element."),
-	JAEXConverterException_String("Problem while convert the element to String value."),
-	JAEXConverterException_Integer("Problem while convert the element to Integer value."),
+	JAEXConverterException_String("Problem while convert the string element."),
+	JAEXConverterException_BigDecimal("Problem while convert the BigDecimal element."),
+	JAEXConverterException_Double("Problem while convert the double element."),
+	JAEXConverterException_Float("Problem while convert the float element."),
+	JAEXConverterException_Integer("Problem while convert the integer element."),
+	JAEXConverterException_Long("Problem while convert the long element."),
+	JAEXConverterException_Boolean("Problem while convert the boolean element."),
 	JAEXConverterException_Date("Problem while convert the Date element, review your decorator mask."),
 	JAEXConverterException_Decorator("Problem while apply the decorator."),
 
