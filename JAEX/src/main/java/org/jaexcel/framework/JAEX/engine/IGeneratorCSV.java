6b009d6108b4795d0501e7522e90cea2c8a9ea07
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-02-01 00:55:29 +0100

    add Interface to Csv generator

---
+++ JAEX/src/main/java/org/jaexcel/framework/JAEX/engine/IGeneratorCSV.java
@@ -0,0 +1,17 @@
+package org.jaexcel.framework.JAEX.engine;
+
+import java.util.List;
+
+public interface IGeneratorCSV {
+
+	
+	void marshalAndSave(Object object, String pathFile, String file) throws Exception;
+	
+	void marshalAsCollectionAndSave(List<Object> listObject, String pathFile, String file) throws Exception;
+	
+	Object unmarshalFromPath(Object object, String pathFile) throws Exception;
+	
+	void unmarshalAsCollectionFromPath(List<Object> listObject, String pathFile, String file) throws Exception;
+	
+	void overrideSepratorChar(char newSeparator);
+}
