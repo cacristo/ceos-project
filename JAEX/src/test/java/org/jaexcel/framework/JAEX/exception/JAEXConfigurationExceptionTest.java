3e44aa0a274c7156fabb1be2628c4858666efc77
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-02-07 20:45:35 +0100

    Upgrade tests

--- JAEX/src/test/java/org/jaexcel/framework/JAEX/exception/JAEXConfigurationExceptionTest.java
+++ JAEX/src/test/java/org/jaexcel/framework/JAEX/exception/JAEXConfigurationExceptionTest.java
@@ -1,8 +1,14 @@
 package org.jaexcel.framework.JAEX.exception;
 
+import org.jaexcel.framework.JAEX.bean.BasicObject;
+import org.jaexcel.framework.JAEX.bean.BasicObjectBuilder;
 import org.jaexcel.framework.JAEX.bean.XlsConfigurationAbsent;
+import org.jaexcel.framework.JAEX.bean.XlsNestedHeaderHorizIncompatible;
+import org.jaexcel.framework.JAEX.bean.XlsNestedHeaderVertiIncompatible;
 import org.jaexcel.framework.JAEX.bean.XlsSheetAbsent;
 import org.jaexcel.framework.JAEX.definition.ExceptionMessage;
+import org.jaexcel.framework.JAEX.engine.CellDecorator;
+import org.jaexcel.framework.JAEX.engine.ConfigCriteria;
 import org.jaexcel.framework.JAEX.engine.Engine;
 import org.jaexcel.framework.JAEX.engine.IEngine;
 
@@ -11,6 +17,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class JAEXConfigurationExceptionTest extends TestCase {
+	
 	private static final String WORKING_DIR = "D:\\projects\\";
 
 	/**
@@ -31,7 +38,8 @@ public class JAEXConfigurationExceptionTest extends TestCase {
 	}
 
 	/**
-	 * Test a configuration exception with missing definitions
+	 * Test a configuration exception, at marshal mode, with missing
+	 * XlsConfiguration definitions
 	 */
 	public void testMarshalMissingXlsConfigurationException() throws Exception {
 		XlsConfigurationAbsent missingConfig = new XlsConfigurationAbsent();
@@ -48,7 +56,8 @@ public class JAEXConfigurationExceptionTest extends TestCase {
 	}
 
 	/**
-	 * Test a configuration exception with missing definitions
+	 * Test a configuration exception, at unmarshal mode, with missing
+	 * XlsConfiguration definitions
 	 */
 	public void testUnmarshalMissingXlsConfigurationException() throws Exception {
 		XlsConfigurationAbsent missingConfig = new XlsConfigurationAbsent();
@@ -65,7 +74,8 @@ public class JAEXConfigurationExceptionTest extends TestCase {
 	}
 
 	/**
-	 * Test a configuration exception with missing definitions
+	 * Test a configuration exception, at marshal mode, with missing XlsSheet
+	 * definitions
 	 */
 	public void testMarshalMissingXlsSheetException() throws Exception {
 		XlsSheetAbsent missingConfig = new XlsSheetAbsent();
@@ -82,7 +92,8 @@ public class JAEXConfigurationExceptionTest extends TestCase {
 	}
 
 	/**
-	 * Test a configuration exception with missing definitions
+	 * Test a configuration exception, at unmarshal mode, with missing XlsSheet
+	 * definitions
 	 */
 	public void testUnmarshalMissingXlsSheetException() throws Exception {
 		XlsSheetAbsent missingConfig = new XlsSheetAbsent();
@@ -99,18 +110,132 @@ public class JAEXConfigurationExceptionTest extends TestCase {
 	}
 
 	/**
-	 * Test a configuration exception incompatible
+	 * Test a horizontal configuration exception conflict
 	 */
-	public void testIncompatibleException() {
-		// FIXME apply test case
-		assertEquals(true, false);
+	public void testHorizontalConflictConfigurationException() {
+		XlsNestedHeaderHorizIncompatible incompatibleConfig = new XlsNestedHeaderHorizIncompatible();
+
+		IEngine en = new Engine();
+		try {
+			en.marshalAndSave(incompatibleConfig, WORKING_DIR);
+		} catch (Exception e) {
+			if (!(e.getClass().equals(ConfigurationException.class)
+					&& e.getMessage().equals(ExceptionMessage.ConfigurationException_Conflict.getMessage()))) {
+				assertEquals(true, false);
+			}
+		}
 	}
 
 	/**
-	 * Test a configuration exception with conflicts
+	 * Test a vertical configuration exception conflict
 	 */
-	public void testConflictException() {
-		// FIXME apply test case
-		assertEquals(true, false);
+	public void testVerticalConflictConfigurationException() {
+		XlsNestedHeaderVertiIncompatible incompatibleConfig = new XlsNestedHeaderVertiIncompatible();
+
+		IEngine en = new Engine();
+		try {
+			en.marshalAndSave(incompatibleConfig, WORKING_DIR);
+		} catch (Exception e) {
+			if (!(e.getClass().equals(ConfigurationException.class)
+					&& e.getMessage().equals(ExceptionMessage.ConfigurationException_Conflict.getMessage()))) {
+				assertEquals(true, false);
+			}
+		}
+	}
+
+	/**
+	 * Test a missing configuration exception at override the header
+	 * {@link CellDecorator}
+	 */
+	public void testOverrideHeaderMissingException() {
+		BasicObject missingConfig = BasicObjectBuilder.buildBasicObject();
+
+		IEngine en = new Engine();
+
+		ConfigCriteria cc = new ConfigCriteria();
+
+		CellDecorator decorator = null;
+		cc.overrideHeaderCellDecorator(decorator);
+
+		try {
+			en.marshalAndSave(cc, missingConfig, WORKING_DIR);
+		} catch (Exception e) {
+			if (!(e.getClass().equals(ConfigurationException.class)
+					&& e.getMessage().equals(ExceptionMessage.ConfigurationException_CellStyleMissing.getMessage()))) {
+				assertEquals(true, false);
+			}
+		}
+	}
+
+	/**
+	 * Test a missing configuration exception at override the numeric
+	 * {@link CellDecorator}
+	 */
+	public void testOverrideNumericMissingException() {
+		BasicObject missingConfig = BasicObjectBuilder.buildBasicObject();
+
+		IEngine en = new Engine();
+
+		ConfigCriteria cc = new ConfigCriteria();
+
+		CellDecorator decorator = null;
+		cc.overrideNumericCellDecorator(decorator);
+
+		try {
+			en.marshalAndSave(cc, missingConfig, WORKING_DIR);
+		} catch (Exception e) {
+			if (!(e.getClass().equals(ConfigurationException.class)
+					&& e.getMessage().equals(ExceptionMessage.ConfigurationException_CellStyleMissing.getMessage()))) {
+				assertEquals(true, false);
+			}
+		}
+	}
+
+	/**
+	 * Test a missing configuration exception at override the boolean
+	 * {@link CellDecorator}
+	 */
+	public void testOverrideBooleanMissingException() {
+		BasicObject missingConfig = BasicObjectBuilder.buildBasicObject();
+
+		IEngine en = new Engine();
+
+		ConfigCriteria cc = new ConfigCriteria();
+
+		CellDecorator decorator = null;
+		cc.overrideBooleanCellDecorator(decorator);
+
+		try {
+			en.marshalAndSave(cc, missingConfig, WORKING_DIR);
+		} catch (Exception e) {
+			if (!(e.getClass().equals(ConfigurationException.class)
+					&& e.getMessage().equals(ExceptionMessage.ConfigurationException_CellStyleMissing.getMessage()))) {
+				assertEquals(true, false);
+			}
+		}
+	}
+
+	/**
+	 * Test a missing configuration exception at override the date
+	 * {@link CellDecorator}
+	 */
+	public void testOverrideDateMissingException() {
+		BasicObject missingConfig = BasicObjectBuilder.buildBasicObject();
+
+		IEngine en = new Engine();
+
+		ConfigCriteria cc = new ConfigCriteria();
+
+		CellDecorator decorator = null;
+		cc.overrideDateCellDecorator(decorator);
+
+		try {
+			en.marshalAndSave(cc, missingConfig, WORKING_DIR);
+		} catch (Exception e) {
+			if (!(e.getClass().equals(ConfigurationException.class)
+					&& e.getMessage().equals(ExceptionMessage.ConfigurationException_CellStyleMissing.getMessage()))) {
+				assertEquals(true, false);
+			}
+		}
 	}
 }
