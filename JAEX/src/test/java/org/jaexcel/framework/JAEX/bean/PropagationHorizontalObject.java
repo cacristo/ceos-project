1bf63778ffc2a4d25cb9b614abfe77e629fb4776
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-02-07 02:12:18 +0100

    Add new objects to test

---
+++ JAEX/src/test/java/org/jaexcel/framework/JAEX/bean/PropagationHorizontalObject.java
@@ -0,0 +1,179 @@
+package org.jaexcel.framework.JAEX.bean;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.jaexcel.framework.JAEX.annotation.XlsConfiguration;
+import org.jaexcel.framework.JAEX.annotation.XlsElement;
+import org.jaexcel.framework.JAEX.annotation.XlsSheet;
+import org.jaexcel.framework.JAEX.definition.ExtensionFileType;
+
+@XlsSheet(title = "Propagation Horizontal Obj")
+@XlsConfiguration(nameFile = "PropagationHorizontalObjects", extensionFile = ExtensionFileType.XLSX)
+public class PropagationHorizontalObject {
+
+	@XlsElement(title = "Date value", position = 1, formatMask = "yyyy-MM-dd", decorator = "anotherDate")
+	private Date dateAttribute;
+
+	@XlsElement(title = "String value", position = 2)
+	private String stringAttribute;
+
+	@XlsElement(title = "Integer value", position = 3)
+	private Integer integerAttribute = 0;
+
+	@XlsElement(title = "Boolean value", position = 4)
+	private Boolean booleanAttribute = Boolean.TRUE;
+
+	@XlsElement(title = "Primitive double value", position = 5)
+	private double doublePrimitiveAttribute = 0;
+
+	@XlsElement(title = "Primitive long value", position = 6)
+	private long longPrimitiveAttribute = 0;
+
+	@XlsElement(title = "Float value", position = 7)
+	private Float floatAttribute = 0f;
+
+	@XlsElement(title = "Unit family", position = 8)
+	private UnitFamily unitFamily;
+
+	@XlsElement(title = "BigDecimal value", position = 9)
+	private BigDecimal bigDecimalAttribute;
+
+	public PropagationHorizontalObject() {
+	}
+
+	/**
+	 * @return the dateAttribute
+	 */
+	public Date getDateAttribute() {
+		return dateAttribute;
+	}
+
+	/**
+	 * @param dateAttribute
+	 *            the dateAttribute to set
+	 */
+	public void setDateAttribute(Date dateAttribute) {
+		this.dateAttribute = dateAttribute;
+	}
+
+	/**
+	 * @return the stringAttribute
+	 */
+	public String getStringAttribute() {
+		return stringAttribute;
+	}
+
+	/**
+	 * @param stringAttribute
+	 *            the stringAttribute to set
+	 */
+	public void setStringAttribute(String stringAttribute) {
+		this.stringAttribute = stringAttribute;
+	}
+
+	/**
+	 * @return the integerAttribute
+	 */
+	public Integer getIntegerAttribute() {
+		return integerAttribute;
+	}
+
+	/**
+	 * @param integerAttribute
+	 *            the integerAttribute to set
+	 */
+	public void setIntegerAttribute(Integer integerAttribute) {
+		this.integerAttribute = integerAttribute;
+	}
+
+	/**
+	 * @return the booleanAttribute
+	 */
+	public Boolean getBooleanAttribute() {
+		return booleanAttribute;
+	}
+
+	/**
+	 * @param booleanAttribute
+	 *            the booleanAttribute to set
+	 */
+	public void setBooleanAttribute(Boolean booleanAttribute) {
+		this.booleanAttribute = booleanAttribute;
+	}
+
+	/**
+	 * @return the doublePrimitiveAttribute
+	 */
+	public double getDoublePrimitiveAttribute() {
+		return doublePrimitiveAttribute;
+	}
+
+	/**
+	 * @param doublePrimitiveAttribute
+	 *            the doublePrimitiveAttribute to set
+	 */
+	public void setDoublePrimitiveAttribute(double doublePrimitiveAttribute) {
+		this.doublePrimitiveAttribute = doublePrimitiveAttribute;
+	}
+
+	/**
+	 * @return the longPrimitiveAttribute
+	 */
+	public long getLongPrimitiveAttribute() {
+		return longPrimitiveAttribute;
+	}
+
+	/**
+	 * @param longPrimitiveAttribute
+	 *            the longPrimitiveAttribute to set
+	 */
+	public void setLongPrimitiveAttribute(long longPrimitiveAttribute) {
+		this.longPrimitiveAttribute = longPrimitiveAttribute;
+	}
+
+	/**
+	 * @return the floatAttribute
+	 */
+	public Float getFloatAttribute() {
+		return floatAttribute;
+	}
+
+	/**
+	 * @param floatAttribute
+	 *            the floatAttribute to set
+	 */
+	public void setFloatAttribute(Float floatAttribute) {
+		this.floatAttribute = floatAttribute;
+	}
+
+	/**
+	 * @return the unitFamily
+	 */
+	public UnitFamily getUnitFamily() {
+		return unitFamily;
+	}
+
+	/**
+	 * @param unitFamily
+	 *            the unitFamily to set
+	 */
+	public void setUnitFamily(UnitFamily unitFamily) {
+		this.unitFamily = unitFamily;
+	}
+
+	/**
+	 * @return the bigDecimalAttribute
+	 */
+	public BigDecimal getBigDecimalAttribute() {
+		return bigDecimalAttribute;
+	}
+
+	/**
+	 * @param bigDecimalAttribute
+	 *            the bigDecimalAttribute to set
+	 */
+	public void setBigDecimalAttribute(BigDecimal bigDecimalAttribute) {
+		this.bigDecimalAttribute = bigDecimalAttribute;
+	}
+}
---
+++ JAEX/src/test/java/org/jaexcel/framework/JAEX/bean/PropagationHorizontalObjectBuilder.java
@@ -0,0 +1,89 @@
+package org.jaexcel.framework.JAEX.bean;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomUtils;
+
+import junit.framework.TestCase;
+
+public class PropagationHorizontalObjectBuilder extends TestCase {
+
+	/**
+	 * Create a PropagationHorizontalObject for tests.
+	 * 
+	 * @return the {@link PropagationHorizontalObject}
+	 */
+	public static PropagationHorizontalObject buildPropagationHorizontalObject() {
+		return buildPropagationHorizontalObject(2);
+	}
+
+	/**
+	 * Create a PropagationHorizontalObject for tests.
+	 * 
+	 * @return the {@link PropagationHorizontalObject}
+	 */
+	public static PropagationHorizontalObject buildPropagationHorizontalObject(int multiplier) {
+		PropagationHorizontalObject toValidate = new PropagationHorizontalObject();
+
+		toValidate.setDateAttribute(new Date());
+		toValidate.setStringAttribute("some string");
+		toValidate.setIntegerAttribute(46 * multiplier);
+		toValidate.setBooleanAttribute(Boolean.FALSE);
+		toValidate.setDoublePrimitiveAttribute(44.6 * multiplier);
+		toValidate.setLongPrimitiveAttribute(987654321L * multiplier);
+		toValidate.setFloatAttribute(14.765f * multiplier);
+		toValidate.setUnitFamily(UnitFamily.COMPONENTS);
+		toValidate.setBigDecimalAttribute(BigDecimal.valueOf(24.777).multiply(BigDecimal.valueOf(multiplier)));
+		// TODO add new fields below
+
+		return toValidate;
+	}
+
+	/**
+	 * Create a list of PropagationHorizontalObject for tests.
+	 * 
+	 * @return the {@link PropagationHorizontalObject}
+	 */
+	public static List<PropagationHorizontalObject> buildListOfPropagationHorizontalObject(int entryNumber) {
+
+		List<PropagationHorizontalObject> returnList = new ArrayList<>();
+		for (int i = 0; i < entryNumber; i++) {
+			returnList.add(buildPropagationHorizontalObject(RandomUtils.nextInt(1, entryNumber)));
+		}
+		return returnList;
+	}
+
+	/**
+	 * Validate the PropagationHorizontalObject based on the object build with the method
+	 * 'buildPropagationHorizontalObject'
+	 * 
+	 * @param toValidate
+	 *            the object to validate
+	 */
+	public static void validatePropagationHorizontalObject(PropagationHorizontalObject toValidate) {
+		PropagationHorizontalObject base = buildPropagationHorizontalObject();
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date());
+
+		Calendar calendarUnmarshal = Calendar.getInstance();
+		calendarUnmarshal.setTime(toValidate.getDateAttribute());
+		assertEquals(calendar.get(Calendar.YEAR), calendarUnmarshal.get(Calendar.YEAR));
+		assertEquals(calendar.get(Calendar.MONTH), calendarUnmarshal.get(Calendar.MONTH));
+		assertEquals(calendar.get(Calendar.DAY_OF_MONTH), calendarUnmarshal.get(Calendar.DAY_OF_MONTH));
+		assertEquals(base.getStringAttribute(), toValidate.getStringAttribute());
+		assertEquals(base.getIntegerAttribute(), toValidate.getIntegerAttribute());
+		assertEquals(base.getBooleanAttribute(), toValidate.getBooleanAttribute());
+		assertEquals(base.getDoublePrimitiveAttribute(), toValidate.getDoublePrimitiveAttribute());
+		assertEquals(base.getLongPrimitiveAttribute(), toValidate.getLongPrimitiveAttribute());
+		assertEquals(base.getFloatAttribute(), toValidate.getFloatAttribute());
+		assertEquals(base.getUnitFamily(), toValidate.getUnitFamily());
+		assertEquals(base.getBigDecimalAttribute(), toValidate.getBigDecimalAttribute());
+		// TODO add new validation below
+	}
+
+}
