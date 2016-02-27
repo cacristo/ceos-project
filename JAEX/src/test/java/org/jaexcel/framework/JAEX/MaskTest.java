518ef0ff46965be5b261d06403f54b2100fe8360
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-02-27 22:43:38 +0100

    Rename test

--- JAEX/src/test/java/org/jaexcel/framework/JAEX/MaskTest.java
+++ JAEX/src/test/java/org/jaexcel/framework/JAEX/MaskTest.java
@@ -9,7 +9,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class ObjectMaskTest extends TestCase {
+public class MaskTest extends TestCase {
 
 	/**
 	 * Create the test case
@@ -17,7 +17,7 @@ public class ObjectMaskTest extends TestCase {
 	 * @param testName
 	 *            name of the test case
 	 */
-	public ObjectMaskTest(String testName) {
+	public MaskTest(String testName) {
 		super(testName);
 	}
 
@@ -25,7 +25,7 @@ public class ObjectMaskTest extends TestCase {
 	 * @return the suite of tests being tested
 	 */
 	public static Test suite() {
-		return new TestSuite(ObjectMaskTest.class);
+		return new TestSuite(MaskTest.class);
 	}
 
 	/**
