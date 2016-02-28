efb9cf17c2646efce13be7847a9cb00d691057d2
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-02-28 19:05:36 +0100

    Add new beans to test package

---
+++ JAEX/src/test/java/org/jaexcel/framework/JAEX/bean/XlsElementOverwriteCell.java
@@ -0,0 +1,72 @@
+package org.jaexcel.framework.JAEX.bean;
+
+import java.util.Date;
+
+import org.jaexcel.framework.JAEX.annotation.XlsConfiguration;
+import org.jaexcel.framework.JAEX.annotation.XlsElement;
+import org.jaexcel.framework.JAEX.annotation.XlsNestedHeader;
+import org.jaexcel.framework.JAEX.annotation.XlsSheet;
+import org.jaexcel.framework.JAEX.definition.ExtensionFileType;
+
+@XlsSheet(title = "Xls Element overwrite")
+@XlsConfiguration(nameFile = "ElementOverwrite", extensionFile = ExtensionFileType.XLSX)
+public class XlsElementOverwriteCell {
+
+	@XlsNestedHeader(title = "Main", startX = 1, endX = 3)
+	@XlsElement(title = "Date value", position = 1)
+	private Date dateAttribute;
+
+	@XlsElement(title = "String value", position = 1, comment = "This is an simple comment")
+	private String stringAttribute;
+
+	@XlsElement(title = "Integer value", position = 2)
+	private Integer integerAttribute = 0;
+
+	public XlsElementOverwriteCell() {
+	}
+
+	/**
+	 * @return the dateAttribute
+	 */
+	public Date getDateAttribute() {
+		return dateAttribute;
+	}
+
+	/**
+	 * @param dateAttribute
+	 *            the dateAttribute to set
+	 */
+	public void setDateAttribute(Date dateAttribute) {
+		this.dateAttribute = dateAttribute;
+	}
+
+	/**
+	 * @return the stringAttribute
+	 */
+	public String getStringAttribute() {
+		return stringAttribute;
+	}
+
+	/**
+	 * @param stringAttribute
+	 *            the stringAttribute to set
+	 */
+	public void setStringAttribute(String stringAttribute) {
+		this.stringAttribute = stringAttribute;
+	}
+
+	/**
+	 * @return the integerAttribute
+	 */
+	public Integer getIntegerAttribute() {
+		return integerAttribute;
+	}
+
+	/**
+	 * @param integerAttribute
+	 *            the integerAttribute to set
+	 */
+	public void setIntegerAttribute(Integer integerAttribute) {
+		this.integerAttribute = integerAttribute;
+	}
+}
---
+++ JAEX/src/test/java/org/jaexcel/framework/JAEX/bean/XlsFreeElementInvalidPositionCell.java
@@ -0,0 +1,117 @@
+package org.jaexcel.framework.JAEX.bean;
+
+import java.util.Date;
+
+import org.jaexcel.framework.JAEX.annotation.XlsConfiguration;
+import org.jaexcel.framework.JAEX.annotation.XlsFreeElement;
+import org.jaexcel.framework.JAEX.annotation.XlsSheet;
+
+@XlsSheet(title = "Xls Free Element invalid position cell")
+@XlsConfiguration(nameFile = "FreeElementInvalid")
+public class XlsFreeElementInvalidPositionCell {
+	@XlsFreeElement(title = "Free String", row = 1, cell = 0)
+	private String freeString;
+
+	@XlsFreeElement(title = "Free Double", row = 2, cell = 1)
+	private Double freeDouble;
+
+	@XlsFreeElement(title = "Free Primitive int", row = 3, cell = 1)
+	private int freePrimitiveInt;
+
+	@XlsFreeElement(title = "Free Date", row = 4, cell = 1)
+	private Date freeDate;
+
+	@XlsFreeElement(title = "Free Long", row = 5, cell = 1)
+	private Long freeLong;
+
+	@XlsFreeElement(title = "Free Primitive Boolean", row = 6, cell = 1)
+	private boolean freePrimitiveBoolean;
+
+	public XlsFreeElementInvalidPositionCell() {
+		super();
+	}
+
+	/**
+	 * @return the freeString
+	 */
+	protected String getFreeString() {
+		return freeString;
+	}
+
+	/**
+	 * @param freeString the freeString to set
+	 */
+	protected void setFreeString(String freeString) {
+		this.freeString = freeString;
+	}
+
+	/**
+	 * @return the freeDouble
+	 */
+	protected Double getFreeDouble() {
+		return freeDouble;
+	}
+
+	/**
+	 * @param freeDouble the freeDouble to set
+	 */
+	protected void setFreeDouble(Double freeDouble) {
+		this.freeDouble = freeDouble;
+	}
+
+	/**
+	 * @return the freePrimitiveInt
+	 */
+	protected int getFreePrimitiveInt() {
+		return freePrimitiveInt;
+	}
+
+	/**
+	 * @param freePrimitiveInt the freePrimitiveInt to set
+	 */
+	protected void setFreePrimitiveInt(int freePrimitiveInt) {
+		this.freePrimitiveInt = freePrimitiveInt;
+	}
+
+	/**
+	 * @return the freeDate
+	 */
+	protected Date getFreeDate() {
+		return freeDate;
+	}
+
+	/**
+	 * @param freeDate the freeDate to set
+	 */
+	protected void setFreeDate(Date freeDate) {
+		this.freeDate = freeDate;
+	}
+
+	/**
+	 * @return the freeLong
+	 */
+	protected Long getFreeLong() {
+		return freeLong;
+	}
+
+	/**
+	 * @param freeLong the freeLong to set
+	 */
+	protected void setFreeLong(Long freeLong) {
+		this.freeLong = freeLong;
+	}
+
+	/**
+	 * @return the freePrimitiveBoolean
+	 */
+	protected boolean isFreePrimitiveBoolean() {
+		return freePrimitiveBoolean;
+	}
+
+	/**
+	 * @param freePrimitiveBoolean the freePrimitiveBoolean to set
+	 */
+	protected void setFreePrimitiveBoolean(boolean freePrimitiveBoolean) {
+		this.freePrimitiveBoolean = freePrimitiveBoolean;
+	}
+}
