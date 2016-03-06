4b17314ef8101d6c8bec29ad1fa4ce6c14c8f549
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-03-06 17:18:20 +0100

    Update comments

--- JAEX/src/test/java/net/ceos/project/poi/annotated/core/PerformanceTest.java
+++ JAEX/src/test/java/net/ceos/project/poi/annotated/core/PerformanceTest.java
@@ -8,18 +8,22 @@ import net.ceos.project.poi.annotated.bean.PerformanceObject;
 import net.ceos.project.poi.annotated.bean.PerformanceObjectBuilder;
 import net.ceos.project.poi.annotated.definition.ExtensionFileType;
 
+/**
+ * Test performance of the library.
+ * 
+ * @version 1.0
+ * @author Carlos CRISTO ABREU
+ */
 public class PerformanceTest {
 
 	/**
-	 * Test one basic object
+	 * Test the performance of generating one file with 5000 lines.
 	 */
 	@Test
 	public void testMarshalMultiObjectPerf() throws Exception {
 		List<PerformanceObject> collection = PerformanceObjectBuilder.buildListOfPerformanceObject(5000);
 
 		IEngine en = new Engine();
-
 		en.marshalAsCollection(collection, "performance_test_list", ExtensionFileType.XLS);
-
 	}
 }
