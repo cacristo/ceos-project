a92badcc5bc1789419d802a2ce58b47d63f41ca4
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-04-20 20:12:26 +0200

    [Master] Add jexan constants

---
+++ jexan/src/main/java/net/ceos/project/poi/annotated/core/Constants.java
@@ -0,0 +1,19 @@
+package net.ceos.project.poi.annotated.core;
+
+/**
+ * Provide all constants inside jexan.
+ * 
+ * @version 1.0
+ * @author Carlos CRISTO ABREU
+ */
+class Constants {
+
+	protected static final String DOT = ".";
+	protected static final String COMMA = ",";
+	protected static final String SLASH = "/";
+
+	protected static final String FORMULA = "formula";
+
+	protected static final String INDY = "idy";
+	protected static final String INDX = "idx";
+}
