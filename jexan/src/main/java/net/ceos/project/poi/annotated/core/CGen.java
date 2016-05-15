a8bd4596193728771712b412226ae8e94a0542a8
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-05-15 19:55:01 +0200

    [8u60] Add license

--- jexan/src/main/java/net/ceos/project/poi/annotated/core/CGen.java
+++ jexan/src/main/java/net/ceos/project/poi/annotated/core/CGen.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 Carlos CRISTO ABREU
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.ceos.project.poi.annotated.core;
 
 import java.io.BufferedReader;
@@ -24,6 +39,12 @@ import net.ceos.project.poi.annotated.exception.CustomizedRulesException;
 import net.ceos.project.poi.annotated.exception.ElementException;
 import net.ceos.project.poi.annotated.exception.WorkbookException;
 
+/**
+ * Engine to manage the CSV files by annotation.
+ * 
+ * @version 1.0
+ * @author Carlos CRISTO ABREU
+ */
 public class CGen implements IGeneratorCSV {
 
 	/**
