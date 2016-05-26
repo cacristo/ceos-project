48c8eccb519711c3633dda1f78ff4542ef282de8
Carlos CRISTO ABREU <cristoabreu@gmail.com>
2016-05-27 00:38:06 +0200

    Add license & JavaDoc

--- jexan/src/test/java/net/ceos/project/poi/annotated/bean/FreeElementObjectBuilder.java
+++ jexan/src/test/java/net/ceos/project/poi/annotated/bean/FreeElementObjectBuilder.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 Carlos CRISTO ABREU
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.ceos.project.poi.annotated.bean;
 
 import static org.testng.Assert.assertEquals;
@@ -9,6 +24,16 @@ import java.util.List;
 
 import org.apache.commons.lang3.RandomUtils;
 
+/**
+ * Builder to:
+ * <li>generate</li>
+ * <li>inject</li>
+ * <li>validate</li><br>
+ * data at the object {@link FreeElementObject}<br>
+ * 
+ * @version 1.0
+ * @author Carlos CRISTO ABREU
+ */
 public class FreeElementObjectBuilder {
 
 	/**
--- jexan/src/test/java/net/ceos/project/poi/annotated/bean/ObjectMaskBuilder.java
+++ jexan/src/test/java/net/ceos/project/poi/annotated/bean/ObjectMaskBuilder.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 Carlos CRISTO ABREU
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.ceos.project.poi.annotated.bean;
 
 import static org.testng.Assert.assertEquals;
@@ -15,6 +30,16 @@ import org.apache.commons.lang3.StringUtils;
 
 import net.ceos.project.poi.annotated.annotation.XlsElement;
 
+/**
+ * Builder to:
+ * <li>generate</li>
+ * <li>inject</li>
+ * <li>validate</li><br>
+ * data at the object {@link ObjectMask}<br>
+ * 
+ * @version 1.0
+ * @author Carlos CRISTO ABREU
+ */
 public class ObjectMaskBuilder {
 
 	/**
